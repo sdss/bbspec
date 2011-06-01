94868e1df5be60a4810764fa2d810e3b92bfbeb0
joelbrownstein <joelbrownstein@AlbireoPro.local>
2011-06-01 22:03:48 +0000

    Added submodule bbspec.spec2d.arcmodel2D

--- python/bbspec/spec2d/__init__.py
+++ python/bbspec/spec2d/__init__.py
@@ -4,4 +4,5 @@ from boltonfuncs import bilinear
 from boltonfuncs import pgh
 from airtovac import a2v as airtovac
 from airtovac import v2a as vactoair
+from arcmodel2D import arcmodel2D
 import psf
