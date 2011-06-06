7a878e3fa8687cd673c661408f43be3a73e78097
StephenBailey <stephenbailey@AlbireoPro.local>
2011-06-06 18:25:30 +0000

    Added code for extracting subregions

--- python/bbspec/spec2d/extract.py
+++ python/bbspec/spec2d/extract.py
@@ -10,7 +10,7 @@ from time import time
 
 import numpy as N
 import scipy.sparse
-from scipy.sparse.linalg import spsolve
+from scipy.sparse import spdiags
 
 from bbspec.spec2d import resolution_from_icov
 
@@ -27,7 +27,14 @@ class Extractor(object):
         self._image_ivar = image_ivar
         self._psf = psf
         
-    def extract():
+        #- Create blank arrays to be filled
+        self._spectra = N.zeros( (psf.nspec, psf.nflux) )
+        self._ivar = N.zeros( (psf.nspec, psf.nflux) )
+        self._deconvolved_spectra = N.zeros( (psf.nspec, psf.nflux) )
+        self._model = N.zeros(image.shape)
+        self._bkg_model = N.zeros(image.shape)
+                
+    def extract(self):
         """
         Perform the extraction and fill in output arrays.
         
@@ -36,31 +43,7 @@ class Extractor(object):
         Sub-classes should implement this.
         """
         raise NotImplementedError
-        
-    def _fill_dummy_values(self):
-        """
-        In the case of bad input, failed extraction, etc., define
-        what the default bad extraction values should be.  For now,
-        let's try zeros (instead of NaN or raising an exception)
-        
-        Note: this won't work for subregions
-        """
-        nspec = self._psf.nspec
-        nflux = self._psf.nflux
-        nx    = self._psf.npix_x
-        ny    = self._psf.npix_y
-        
-        #- use the same zeros arrays to not waste memory
-        blank_spec = N.zeros( (nspec, nflux) )
-        self._spectra = blank_spec
-        self._ivar = blank_spec
-        self._deconvolved_spectra = blank_spec
-        
-        n = nspec*nflux
-        blank_cov = N.zeros( (n,n) )
-        self._resolution = blank_cov
-        self._dspec_icov = blank_cov
-        
+                
     #- python properties are a way to implement read-only attributes
     #- for objects.  The following code is a way to put in the base
     #- class that all Extractors should have X.spectra, etc. without
@@ -107,6 +90,20 @@ class Extractor(object):
         """
         return self._dspec_icov
 
+    @property
+    def model(self):
+        """
+        Model image from extracted spectra
+        """
+        return self._model
+
+    @property
+    def bkg_model(self):
+        """
+        Model image from extracted spectra
+        """
+        return self._bkg_model
+
 #-------------------------------------------------------------------------
 
 _t0 = None
@@ -144,6 +141,119 @@ class SimpleExtractor(Extractor):
         """ 
         #- Call Extractor.__init__ to actually do the initialization
         super(SimpleExtractor, self).__init__(image, image_ivar, psf)
+        
+    def _add_legendre(self, A, nx, ny, maxorder=5):
+        """
+        extend projection matrix A with terms for scattered light
+        
+        returns Ax[npix, nflux+maxorder+1]
+        """
+        #- Try legendre polynomials in x-direction only
+        x = N.tile(N.linspace(-1, 1, nx), ny)
+        
+        B = N.zeros( (ny*nx, maxorder+1) )
+        for i in range(maxorder+1):
+            B[:, i] = scipy.special.legendre(i)(x)
+            
+        Ax = scipy.sparse.hstack( (A, B) )
+        return Ax
+
+    def extract_subregion(self, speclo, spechi, fluxlo, fluxhi, bkgorder=None):
+        """
+        Extract just a subset of the spectra instead of all of them
+        """
+
+        #- Find which pixels are covered by these spectra
+        psf = self._psf
+        xlo = max(0, int(psf.param['X'][speclo, fluxlo:fluxhi].min()))
+        xhi = min(psf.npix_x, int(psf.param['X'][spechi-1, fluxlo:fluxhi].max()))
+        ylo = max(0, int(psf.param['Y'][speclo:spechi, fluxlo].min()))
+        yhi = min(psf.npix_y, int(psf.param['Y'][speclo:spechi, fluxhi-1].max()))
+        
+        #- Experimental: Add some boundary in x and y
+        xlo = max(0, xlo-8)
+        xhi = min(xhi+8, psf.npix_x)
+        
+        ylo = max(0, ylo-5)
+        yhi = min(yhi+5, psf.npix_y)
+        
+        nx = xhi - xlo
+        ny = yhi - ylo
+
+        #- Extract a larger range in wavelength, then trim down later
+        dflo = min(fluxlo, 10)
+        dfhi = min(psf.nflux-fluxhi, 10)
+        
+        nspec = spechi - speclo
+        nflux = fluxhi - fluxlo
+                
+        #- Get sparse projection matrix A
+        A = psf.getSparseAsub(speclo, spechi, fluxlo-dflo, fluxhi+dfhi, \
+                              xlo, xhi, ylo, yhi)
+        image = self._image[ylo:yhi, xlo:xhi]
+        ivar = self._image_ivar[ylo:yhi, xlo:xhi]
+
+        #- TEST : add background legendre polynomial
+        if bkgorder >= 0:
+            A = self._add_legendre(A, nx, ny, maxorder=bkgorder)
+
+        #- Noise weight A and pixels
+        #- Ni = (N)^-1
+        Ni = spdiags(N.sqrt(ivar.ravel()), [0,], A.shape[0], A.shape[0])
+        NiA = Ni.dot(A)
+        p = (image*N.sqrt(ivar)).ravel()
+        
+        #- Spectra extend beyond pixels, so add a normalization term to
+        #- avoid singularity: unconstrained spectra should -> 0
+        npix, nfluxbins = A.shape
+        I = 1e-6 * scipy.sparse.identity(nfluxbins)
+        
+        NiAx = scipy.sparse.vstack((NiA, I))
+        Ax = scipy.sparse.vstack((A, I))
+        p = N.concatenate( (p, N.zeros(nfluxbins)) )
+        
+        ATA = Ax.T.dot( NiAx )
+        ATA = N.array(ATA.todense())
+        try:
+            ATAinv = N.linalg.inv(ATA)
+        except N.linalg.LinAlgError, e:
+            print >> sys.stderr, e
+            print >> sys.stderr, "ERROR: Can't invert matrix"
+            return
+            
+        xspec0 = N.array( ATAinv.dot( Ax.T.dot(p) ) )
+
+        #- Project model, including bkg terms
+        model = A.dot(xspec0.ravel()).reshape(ny, nx)
+        self._model[ylo:yhi, xlo:xhi] = model
+        
+        #- Project just the bkg model part
+        if bkgorder > 0:
+            bkg = N.zeros(xspec0.size)
+            bkg[-bkgorder-1:] = xspec0.ravel()[-bkgorder-1:]
+            bkg_model = A.dot(bkg).reshape(ny, nx)
+            self._bkg_model[ylo:yhi, xlo:xhi] = bkg_model
+                    
+        xspec0 = xspec0.reshape( (nspec, nflux+dflo+dfhi) )
+        
+        #- Reconvolve flux by resolution
+        R = resolution_from_icov(ATA)
+        xspec1 = N.dot(R, xspec0.ravel()).reshape(xspec0.shape)
+
+        #- Variance of extracted reconvolved spectrum
+        Cx = N.dot(R, N.dot(ATAinv, R.T))  #- Bolton & Schelgel 2010 Eqn 15
+        xspec_var = N.array([Cx[i,i] for i in range(Cx.shape[0])])
+        xspec_ivar = (1.0/xspec_var).reshape(xspec1.shape)
+        
+        #- Rename working variables for output
+        self._spectra[speclo:spechi, fluxlo:fluxhi] = xspec1[:, dflo:dflo+nflux]
+        self._ivar[speclo:spechi, fluxlo:fluxhi] = xspec_ivar[:, dflo:dflo+nflux]
+        self._deconvolved_spectra[speclo:spechi, fluxlo:fluxhi] = xspec0[:, dflo:dflo+nflux]
+                                
+        #- What do I fill in for these?
+        # n = psf.nspec * psf.nflux
+        # self._dspec_icov = ATA.reshape( (n, n) )
+        # self._resolution = R
 
     def extract(self):
         """Actually do the extraction"""
@@ -151,7 +261,6 @@ class SimpleExtractor(Extractor):
         
         if N.all(self._image_ivar == 0.0):
             print >> sys.stderr, "ERROR: input ivar all 0"
-            self._fill_dummy_values()
             return
 
         #- shortcuts
@@ -164,7 +273,7 @@ class SimpleExtractor(Extractor):
         #- Generate the matrix to solve
         _timeit()
         Ax = psf.getSparseA()
-        _timeit('Get Sparse A')
+        ### _timeit('Get Sparse A')
         
         ixy = N.arange(len(sqrt_ivar))
         npix = psf.npix_x * psf.npix_y
@@ -180,7 +289,6 @@ class SimpleExtractor(Extractor):
         except N.linalg.LinAlgError, e:
             print >> sys.stderr, e
             print >> sys.stderr, "ERROR: Can't invert matrix"
-            self._fill_dummy_values()
             return
             
         _timeit('Invert ATA')
@@ -194,9 +302,9 @@ class SimpleExtractor(Extractor):
         #- Pulling out diagonal blocks doesn't seem to work (for numerical reasons?),
         #- so use full A.T A matrix
         R = resolution_from_icov(ATA)
-        _timeit('Find R')
+        ### _timeit('Find R')
         xspec1 = N.dot(R, xspec0.ravel()).reshape( (psf.nspec, psf.nflux) )
-        _timeit('Reconvolve')
+        ### _timeit('Reconvolve')
 
         #- Variance of extracted reconvolved spectrum
         Cx = N.dot(R, N.dot(ATAinv, R.T))  #- Bolton & Schelgel 2010 Eqn 15
